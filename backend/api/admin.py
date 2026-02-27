7175c7f65d17ae8677b420b8244f377696238f79
Neilsen <nmrubix11@gmail.com>
2026-02-27 13:11:41 -0700

    build.sh

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,4 +1,3 @@
-  
 import os
 from flask_admin import Admin
 from .models import db, User
@@ -6,9 +5,11 @@ from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
     app.secret_key = os.environ.get('FLASK_APP_KEY', 'sample key')
-    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-    admin = Admin(app, name='4Geeks Admin', template_mode='bootstrap3')
-
     
-   
+    app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
+    
+    admin = Admin(app, name='4Geeks Admin') 
+    
     admin.add_view(ModelView(User, db.session))
+    
+    return admin
